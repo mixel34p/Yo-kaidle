b0c740329954095fe79b1b44ddae571b96368ea7
Mixel <82977116+mixel34p@users.noreply.github.com>
2025-05-25 22:44:53 +0200

    Version 1.0.8.2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     domains: ['supabase.co', 'yokaiwatch.fandom.com', 'static.wikia.nocookie.net'],
   },
